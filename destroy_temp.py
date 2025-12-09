23f978cc1add2392ffc52e59332ed164bdc0acfa
Mohamed <hrbim756@gmail.com>
2025-12-09 06:33:59 +0100

    feat: add dashboard page with deployment statistics, list, and client-side interactions.

--- destroy_temp.py
+++
@@ -1,8 +0,0 @@
-from python_terraform import Terraform
-from pathlib import Path
-
-terraform_exe = str(Path('./terraform_bin/terraform.exe').absolute())
-tf = Terraform(working_dir='./terraform', terraform_bin_path=terraform_exe)
-tf.workspace('select', 'frontend_ecommerce')
-result = tf.destroy(auto_approve=True)
-print(result)
